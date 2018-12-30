feb2939239a680d0138ec78a38838f7eb703bdf4
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-12-30 12:02:19 +0100

    Update make-release.sh

--- make-release.sh
+++ make-release.sh
@@ -7,16 +7,16 @@ echo "Preparing a new release..."
 git clone https://github.com/$REPO_OWNER_NAME/$REPO
 git config --global user.name "$REPO_OWNER_NAME"
 git config --global user.email "$REPO_OWNER_EMAIL"
-git config --global user.password "$REPO_OWNER_PASSWORD"
+git remote add myrepo https://$REPO_OWNER_NAME:$REPO_OWNER_PASSWORD@github.com/$REPO_OWNER_NAME/$REPO
 cd $REPO
 mvn versions:set -DremoveSnapshot=true
 git commit pom.xml -m "[YE-0] Release"
 git checkout master
 git merge -X theirs develop
-git push origin master
+git push myrepo master
 git checkout develop
 mvn versions:set -DnextSnapshot=true
 git commit pom.xml -m "[YE-0] Prepare next development version."
-git push
+git push myrepo develop
 
 echo "Release complete."
