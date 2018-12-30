ad095262939cd806ea203c1c77f65a483cfd9ee1
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-12-30 10:34:37 +0100

    Create make-release.sh

---
+++ make-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source release.txt
+
+echo "Preparing a new release..."
+
+git clone https://$REPO
+mvn versions:set -DremoveSnapshot=true
+git commit pom.xml -m "[YE-0] Release"
+git checkout master
+git merge -X theirs develop
+git push origin master
+git checkout develop
+mvn versions:set -DnextSnapshot=true
+git commit pom.xml -m "[YE-0] Prepare next development version."
+git push
+
+echo "Release complete."
