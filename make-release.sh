21e6fe1189c46d6df46108e57e3ea6183ce0e3af
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-12-31 13:42:20 +0100

    Update make-release.sh

--- make-release.sh
+++ make-release.sh
@@ -15,8 +15,8 @@ git remote add myrepo https://$GH_TOKEN@github.com/$REPO_OWNER_NAME/$REPO
 echo "----------------------------------------------------------------"
 echo "                   Set new release version.                     "
 echo "----------------------------------------------------------------"
-mvn versions:set -DremoveSnapshot -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
-git commit pom.xml -m "[YE-0] Release"
+mvn versions:set -DnewVersion=$RELEASE_VERSION -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+git commit pom.xml -m "[YE-0] Release $RELEASE_VERSION"
 echo "----------------------------------------------------------------"
 echo "                   Checkout master branch.                      "
 echo "----------------------------------------------------------------"
@@ -30,8 +30,8 @@ git checkout develop
 echo "----------------------------------------------------------------"
 echo "                Set new snapshot version.                       "
 echo "----------------------------------------------------------------"
-mvn versions:set -DnextSnapshot -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
-git commit pom.xml -m "[YE-0] Prepare next development version."
+mvn versions:set -DnewVersion=$DEV_VERSION -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+git commit pom.xml -m "[YE-0] Prepare next development version $DEV_VERSION."
 git push --set-upstream myrepo develop 
 
 echo "Release complete."
