a1e19bef4527aa6a0505d8982b273384a8ac0c24
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-12-31 09:23:05 +0100

    Update make-release.sh

--- make-release.sh
+++ make-release.sh
@@ -9,14 +9,14 @@ git config --global user.name "$REPO_OWNER_NAME"
 git config --global user.email "$REPO_OWNER_EMAIL"
 cd $REPO
 git remote add myrepo https://$GH_TOKEN@github.com/$REPO_OWNER_NAME/$REPO
-mvn versions:set -B -q -DremoveSnapshot=true
+mvn versions:set -DremoveSnapshot=true
 VERSION=`cd $project_loc && mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
 git commit pom.xml -m "[YE-0] Release $VERSION"
 git checkout master
 git merge -X theirs develop
 git push --quiet --set-upstream myrepo master 
 git checkout develop
-mvn versions:set -B -q -DnextSnapshot=true
+mvn versions:set -DnextSnapshot=true
 git commit pom.xml -m "[YE-0] Prepare next development version."
 git push --quiet --set-upstream myrepo develop 
 
