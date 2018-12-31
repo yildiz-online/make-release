cbf7d06574ac8111cb15a3ffc54889da699de570
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-12-31 09:09:50 +0100

    Update make-release.sh

--- make-release.sh
+++ make-release.sh
@@ -9,13 +9,13 @@ git config --global user.name "$REPO_OWNER_NAME"
 git config --global user.email "$REPO_OWNER_EMAIL"
 cd $REPO
 git remote add myrepo https://$GH_TOKEN@github.com/$REPO_OWNER_NAME/$REPO
-mvn versions:set -DremoveSnapshot=true
+mvn versions:set -B -DremoveSnapshot=true
 git commit pom.xml -m "[YE-0] Release"
 git checkout master
 git merge -X theirs develop
 git push --quiet --set-upstream myrepo master 
 git checkout develop
-mvn versions:set -DnextSnapshot=true
+mvn versions:set -B -DnextSnapshot=true
 git commit pom.xml -m "[YE-0] Prepare next development version."
 git push --quiet --set-upstream myrepo develop 
 
