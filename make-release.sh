90bd050df0d90b77a4184929991ab108915f93a5
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-08 20:26:54 +0100

    [YE-0] Fix build.

--- make-release.sh
+++ make-release.sh
@@ -6,32 +6,32 @@ echo "----------------------------------------------------------------"
 echo "                     Cloninig repository.                       "
 echo "----------------------------------------------------------------"
 
-git clone https://github.com/$REPO_OWNER_NAME/$REPO --quiet
+git clone https://github.com/$REPO_OWNER_NAME/$REPO
 cd $REPO
 echo "----------------------------------------------------------------"
 echo "                        Adding remote.                          "
 echo "----------------------------------------------------------------"
-git remote add myrepo https://$GH_TOKEN@github.com/$REPO_OWNER_NAME/$REPO --quiet
+git remote add myrepo https://$GH_TOKEN@github.com/$REPO_OWNER_NAME/$REPO
 echo "----------------------------------------------------------------"
 echo "                   Set new release version.                     "
 echo "----------------------------------------------------------------"
 mvn versions:set -DnewVersion=$RELEASE_VERSION -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
-git commit pom.xml -m "[YE-0] Release $RELEASE_VERSION" --quiet
+git commit pom.xml -m "[YE-0] Release $RELEASE_VERSION"
 echo "----------------------------------------------------------------"
 echo "                   Checkout master branch.                      "
 echo "----------------------------------------------------------------"
-git checkout master --quiet
-git merge -X theirs develop --quiet
-git push --set-upstream myrepo master --quiet
+git checkout master
+git merge -X theirs develop
+git push --set-upstream myrepo master
 echo "----------------------------------------------------------------"
 echo "                  Checkout develop branch.                      "
 echo "----------------------------------------------------------------"
-git checkout develop --quiet
+git checkout develop
 echo "----------------------------------------------------------------"
 echo "                Set new snapshot version.                       "
 echo "----------------------------------------------------------------"
 mvn versions:set -DnewVersion=$DEV_VERSION -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
-git commit pom.xml -m "[YE-0] Prepare next development version $DEV_VERSION." --quiet
-git push --set-upstream myrepo develop --quiet
+git commit pom.xml -m "[YE-0] Prepare next development version $DEV_VERSION."
+git push --set-upstream myrepo develop
 
 echo "Release complete."
